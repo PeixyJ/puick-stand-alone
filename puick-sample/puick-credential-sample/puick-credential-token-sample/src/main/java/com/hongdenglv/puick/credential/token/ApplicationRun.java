b5d89587877f2196480d7c7826f2746251770000
peixinyi <peixy_j@163.com>
2022-04-14 23:58:14 +0800

    feat: puick-credential-token-sample 范例项目

---
+++ puick-sample/puick-credential-sample/puick-credential-token-sample/src/main/java/com/hongdenglv/puick/credential/token/ApplicationRun.java
@@ -0,0 +1,19 @@
+package com.hongdenglv.puick.credential.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动
+ *
+ * @author PeiXy_J
+ * @date 2022-4-14 23:20
+ */
+@SpringBootApplication
+public class ApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+    }
+
+}
